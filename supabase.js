4425df6303b1a4e6d2fc01a73736ce1b4c01f4c0
OUPS! Festival <manuelbischof.phil@gmail.com>
2026-06-26 16:23:08 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -7,3 +7,17 @@ const supabaseClient = supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
+async function testSupabase(){
+
+const {data,error}=await supabaseClient
+.from("articles")
+.select("*");
+
+
+console.log(data);
+console.log(error);
+
+}
+
+
+testSupabase();

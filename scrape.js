ed05eb2c9ff0ac9d1b6d93b8b74e8ac4f7c38e25
OUPS! Festival <manuelbischof.phil@gmail.com>
2026-06-26 08:34:51 +0200

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -4,6 +4,8 @@ const API =
 
 let articles = [];
 
+let selectedArticle = null;
+
 
 const articleList =
 document.getElementById("articleList");
@@ -16,44 +18,154 @@ async function loadArticles(){
 
     articles = await response.json();
 
-    console.log(articles);
-
-    showArticles();
+    renderArticles();
 
 }
 
 
 
-function showArticles(){
+function renderArticles(){
 
-    articleList.innerHTML="";
+    articleList.innerHTML = "";
 
 
     articles.forEach(article=>{
 
 
-        const item=document.createElement("div");
+        const div=document.createElement("div");
+
+        div.className="article";
 
 
-        item.className="article";
+        div.innerHTML=`
 
+            <strong>${article.title}</strong>
 
-        item.innerHTML=`
+            <br>
 
-            <h3>${article.title}</h3>
-
-            <p>${article.category || ""}</p>
+            <small>
+            ${article.category || ""}
+            </small>
 
         `;
 
 
-        articleList.appendChild(item);
+        div.onclick=()=>{
+
+            selectArticle(article);
+
+        };
+
+
+        articleList.appendChild(div);
 
 
     });
 
+}
+
+
+
+function selectArticle(article){
+
+    selectedArticle = article;
+
+
+    document.getElementById("title").value =
+    article.title || "";
+
+
+    document.getElementById("category").value =
+    article.category || "";
+
+
+    document.getElementById("tags").value =
+    article.tags || "";
+
+
+    document.getElementById("excerpt").value =
+    article.excerpt || "";
+
+
+    document.getElementById("content").value =
+    article.content || "";
 
 }
+
+
+
+document
+.getElementById("newArticle")
+.onclick=()=>{
+
+
+    selectedArticle=null;
+
+
+    document
+    .querySelectorAll("input, textarea")
+    .forEach(el=>el.value="");
+
+
+};
+
+
+
+document
+.getElementById("save")
+.onclick=async()=>{
+
+
+    const article={
+
+        slug:
+        selectedArticle
+        ? selectedArticle.slug
+        : Date.now().toString(),
+
+
+        title:
+        document.getElementById("title").value,
+
+
+        category:
+        document.getElementById("category").value,
+
+
+        excerpt:
+        document.getElementById("excerpt").value,
+
+
+        content:
+        document.getElementById("content").value,
+
+
+        tags:
+        document.getElementById("tags").value
+
+    };
+
+
+    await fetch(API,{
+
+        method:"POST",
+
+        headers:{
+            "Content-Type":"application/json"
+        },
+
+        body:
+        JSON.stringify(article)
+
+    });
+
+
+    alert("Saved");
+
+    loadArticles();
+
+
+};
 
 
 

8d82cff333413ffa2ecc1f277106ade84a27e5c8
OUPS! Festival <manuelbischof.phil@gmail.com>
2026-06-26 20:17:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -177,8 +177,8 @@ function openArticle(article){
   document.getElementById("articleCategory").textContent =
     article.category;
 
-  document.getElementById("articleTags").innerHTML =
-  article.tags.map(tag => 
+ document.getElementById("articleTags").innerHTML =
+  (article.tags || []).map(tag => 
     `<span class="article-tag">${tag}</span>`
   ).join(" ");
 
